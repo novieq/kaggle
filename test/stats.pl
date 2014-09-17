7e2a907ce2a05d4007ce67087785650a8677c570
Sayantan <sayghosh@ebay.com>
2014-09-16 21:49:16 -0700

    Used an array of hashes. Each element of the hash corresponds to a categorical variable. The keys for a categorical variable(array index), are the different values of the categorical variable and the value of the key is the impression count

--- test/stats.pl
+++ test/stats.pl
@@ -4,7 +4,18 @@ my $usage = "$0 <train.csv>";
 my $fileName = shift @ARGV || die $usage;
 open(INFILE, "$fileName") || die "Cannot open $fileName";
 my $line = <INFILE>;
-while($line = <INFILE>) {
-	my @varBuckets = split(',', $line);
+#Id,Label,I1,I2,I3,I4,I5,I6,I7,I8,I9,I10,I11,I12,I13,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,C14,C15,C16,C17,C18,C19,C20,C21,C22,C23,C24,C25,C26
+my @aoh; #Array of hashes. Array location 0 is for the hash containing the categorical values of C1. 
 
+while($line = <INFILE>) {
+	my @tokens = split(',', $line);
+	foreach my $i (1..26) { #$i loops from C1 to C26 iterating over the respective hashes
+		chomp $tokens[14+$i];
+		my ($imp,$click)=(1,$tokens[1]);
+		$aoh[$i]{$tokens[14+$i]}=$aoh[$i]{$tokens[14+$i]}+($imp,$click); #Adding impressions for every categorical value of Ci
+	}
+}
+
+foreach my $i (1..26) {
+	while (my ($k,$v)=each $aoh[$i]){print "C${i}I $k $v\n"};
 }
