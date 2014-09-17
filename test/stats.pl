e7f37fdcd7648a8e3f845c93950d925414a58423
novieq <kx.learner@gmail.com>
2014-09-16 22:23:53 -0700

    Stats

--- test/stats.pl
+++ test/stats.pl
@@ -7,16 +7,17 @@ open(INFILE, "$fileName") || die "Cannot open $fileName";
 my $line = <INFILE>;
 #Id,Label,I1,I2,I3,I4,I5,I6,I7,I8,I9,I10,I11,I12,I13,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,C14,C15,C16,C17,C18,C19,C20,C21,C22,C23,C24,C25,C26
 my @aoh; #Array of hashes. Array location 0 is for the hash containing the categorical values of C1. 
+my @aohClicks;
 
 while($line = <INFILE>) {
 	my @tokens = split(',', $line);
 	foreach my $i (1..26) { #$i loops from C1 to C26 iterating over the respective hashes
 		chomp $tokens[14+$i];
-		my ($imp,$click)=(1,$tokens[1]);
-		$aoh[$i]{$tokens[14+$i]}=$aoh[$i]{$tokens[14+$i]}+($imp,$click); #Adding impressions for every categorical value of Ci
+		$aoh[$i]{$tokens[14+$i]}=$aoh[$i]{$tokens[14+$i]}+1; #Adding impressions for every categorical value of Ci
+		$aohClicks[$i]{$tokens[14+$i]}=$aohClicks[$i]{$tokens[14+$i]}+$tokens[1];
 	}
 }
 
 foreach my $i (1..26) {
-	while (my ($k,$v)=each $aoh[$i]){print "C${i}I $k $v\n"};
+	while (my ($k,$v)=each $aoh[$i]){print "C${i},$k,$v,$aohClicks[$i]{$k}\n"};
 }
